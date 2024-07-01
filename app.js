7fb59dda2df99c8dabda83e849969a3af9092400
Ozgur Arslan <blueorange589@gmail.com>
2024-07-01 14:04:35 +0300

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 const btn = jsvg.button({
   stretch: false,
   size: 'md',
@@ -15,3 +12,28 @@ const btnblue = jsvg.button({
   stroke: 'red',
   'stroke-width': 2
 })
+
+const layout = {
+  'header': {
+    'navbar': {
+      fill: 'blue',
+      'font-color': 'white',
+      contains: {
+        'logo': {
+          'fill': 'black',
+          'align': 'left'
+        },
+        'searchbar': {
+          'align': 'center'
+        },
+        'burger': {
+          fill: 'pink',
+          'align': 'right',
+          events: {
+            click: app.displayMenu
+          }
+        },
+      }
+    }
+  }
+}
