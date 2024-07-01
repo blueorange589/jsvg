c87d4318732c6b81a3f9c4737c81751186b44a93
Ozgur Arslan <blueorange589@gmail.com>
2024-07-01 13:51:49 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+const btn = jsvg.button({
+  stretch: false,
+  size: 'md',
+  'stroke-width': 1
+})
+
+const btnblue = jsvg.button({
+  fill: 'blue',
+  'font-color': 'white',
+  stretch: false,
+  size: 'md',
+  stroke: 'red',
+  'stroke-width': 2
+})
